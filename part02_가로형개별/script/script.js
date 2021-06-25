aec70a42fb726c55d51a4e7e1aacd6115669d2f6
sun980618 <koarera98@gmail.com>
2021-06-25 14:52:49 +0900

    part02_가로형개별GNB추가

--- part02_가로형개별/script/script.js
+++ part02_가로형개별/script/script.js
@@ -2,15 +2,11 @@
 
 $(function(){
    $("li.mainLi").mouseover(function(){
-
-       $(this).children("ul.subMenu").stop().slideDown(800);
-       /*가로전체형 코드$("ul.subMenu").stop().slideDown(800);*/
+       $("ul.subMenu").stop().slideDown(800);
    });
 
     $("li.mainLi").mouseout(function(){
-
-        $(this).children("ul.subMenu").stop().slideUp(300);
-        /*가로전체형 코드$("ul.subMenu").stop().slideUp(300)*/
+        $("ul.subMenu").stop().slideUp(300)
     });
 
     /*.stop() 매서드는 메모리 유지기능을 소멸시켜서 사용자의 이벤트가 종료되면 기능 또한 즉시 멈추는 기능*/
